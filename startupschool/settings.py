3601ff98584439b29fecd1e4d510a418f51c1155
cck <gun519@likelion.org>
2022-11-30 11:50:28 +0900

    hello, chang!

--- startupschool/settings.py
+++ startupschool/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ["https://*.cloutype.app"]
+CSRF_TRUSTED_ORIGINS = ["https://*.cloudtype.app"]
 
 
 
